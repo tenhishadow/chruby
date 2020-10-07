bfdf714aa1b20ad6014ec6c02e4b6a7a2380af2f
Postmodern <postmodern.mod3@gmail.com>
2020-10-07 16:40:07 -0700

    Renamed chruby_use to chruby_set.

--- test/chruby_set_test.sh
+++ test/chruby_set_test.sh
@@ -2,7 +2,7 @@
 
 function setUp()
 {
-	chruby_use "$test_ruby_root" >/dev/null
+	chruby_set "$test_ruby_root" >/dev/null
 }
 
 function test_chruby_clears_hash_table()
@@ -16,14 +16,14 @@ function test_chruby_clears_hash_table()
 	fi
 }
 
-function test_chruby_use_with_invalid_ruby_path()
+function test_chruby_set_with_invalid_ruby_path()
 {
-	chruby_use "/path/to/fake/ruby" 2>/dev/null
+	chruby_set "/path/to/fake/ruby" 2>/dev/null
 
 	assertEquals "did not return an error code" 1 $?
 }
 
-function test_chruby_use_env_variables()
+function test_chruby_set_env_variables()
 {
 	assertEquals "invalid RUBY_ROOT"    "$test_ruby_root" "$RUBY_ROOT"
 	assertEquals "invalid RUBY_ENGINE"  "$test_ruby_engine" "$RUBY_ENGINE"
